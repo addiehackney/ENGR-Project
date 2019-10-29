caba94960bc4174035fa55a88a975f2f1c4bc4fd
SmithCollins2k <57153062+SmithCollins2k@users.noreply.github.com>
2019-10-29 14:50:49 -0500

    Add files via upload

--- DiseaseSpread/EuropeMap.java
+++ DiseaseSpread/EuropeMap.java
@@ -16,6 +16,6 @@ public class EuropeMap extends World
     public EuropeMap()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1900, 1625, 1); 
     }
 }
